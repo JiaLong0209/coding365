3abed0b0ec9e05313258c4c23507ad2e98ab1b3b
Jialong0209 <smt02091395@gmail.com>
2022-09-25 00:48:09 +0800

    0924

--- practice/0918~/stars2.js
+++ practice/0918~/stars2.js
@@ -1,13 +1,60 @@
 
 
-function start2(num){
-
-    
+function stars2(num){
+    console.log(num)
+    for(let i = 0; i < num*2-1; i++){
+        let str = "";
+        for(let j = num - i -1; j < num; j++){
+            if(i > num-1){
+                for(let k = i; k < num*2-1; k++){
+                    str += "*";
+                }
+            break;
+            }else{
+                str += "*";
+            }
+        }
+        console.log(str);
+    }
+    console.log("\n");
 }
 
+function stars2_Arr(num){
+    console.log(num)
+    let str = "";
+    let strArr = [];
+    for(let i = 0; i < num; i++){
+        str += "*";
+        if(i != num){
+            strArr.push(str)
+        }
+        console.log(str);
 
+    }
+    for(let i = strArr.length-1; i > -1; i--){
+        console.log(strArr[i]);
+    }
+    console.log("\n")
+}
 
-start2(2);
+// start2(2);
 // *
+// **
+// *
+let amount = 2000;
+
+let start1 = new Date();
+stars2(amount);
+let end1 = new Date();
+
+let start2 = new Date();
+stars2_Arr(amount);
+let end2 = new Date();
+
+console.log(`stars1 execute time : ${(end1 - start1)/1000} s`);
+console.log(`stars2 execute time : ${(end2 - start2)/1000} s`);
+// *
+// **
+// ***
 // **
 // *
