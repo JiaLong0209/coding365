07480e0f8651111a5d943b328d91afc6cdb2f91b
Jialong0209 <smt02091395@gmail.com>
2022-10-06 21:16:31 +0800

    1006 stackLinkList

--- practice/1004~/stackLinkList.js
+++ practice/1004~/stackLinkList.js
@@ -29,22 +29,68 @@ class LinkList{
         }
         this.size -= 1;
     }
-    printList(){
+    printList(reverse = 0){
         let current = this.head;
-        for(let i = 0; i < this.size; current = current? current.next: null){
-            if(current){
-                console.log(current.data)
-            }else{
-                break;
+        if(!reverse){
+            for(let i = 0; i < this.size; current = current.next, i++){
+                console.log(`${i}: ${current.data}`)
+            }
+        }else{
+            for(let i = this.size; i > 0; current = current.next, i--){
+                console.log(`${i-1}: ${current.data}`)
             }
         }
+    }
+    forEach(callback){
+        let current = this.head;
+        for(let i = 0; i < this.size; i++){
+            callback(current);   
+            current = current.next;
+        }
+
+    }
+
+};
+
+class Stack{
+    constructor(){
+        this.top = 0;
+        this.stack = new LinkList();
+    }
+    push(data){
+        this.stack.inserFirst(data);
+        this.top += 1;
+    }
+    pop(){
+        this.stack.removeAtIndex(0);
+        this.top -= 1;
+    }
+    printStack(){
+        this.stack.printList(1);
+    }
+
+    forEach(callback){
+        this.stack.forEach(callback);
     }
 
 };
 
 let a = new LinkList();
-// a.inserFirst(100);
-// a.inserFirst(200);
-// a.inserFirst(300);
-// a.removeAtIndex(1);
-// a.printList();
+console.log(` LinkList : \n`)
+a.inserFirst(100);
+a.inserFirst(200);
+a.inserFirst(300);
+a.removeAtIndex(0);
+a.inserFirst(400);
+a.forEach((x)=>{x.data *= 10})
+a.printList();
+
+let b = new Stack();
+console.log(`\nStack : \n`)
+b.push(100);
+b.push(200);
+b.push(300);
+b.pop();
+b.push(400)
+b.forEach(x=>{x.data *= 10})
+b.printStack();
