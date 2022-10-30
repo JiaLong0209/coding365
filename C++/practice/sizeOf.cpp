b92a5a8aa0e16af127bc13d145bd3ff48239344a
Jialong0209 <smt02091395@gmail.com>
2022-10-30 15:23:58 +0800

    1030 sizeof c++

---
+++ C++/practice/sizeOf.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    // unsigned long a;
+    long long int a;
+    long long b;
+    double c;
+    long double d = 342;
+    cout << sizeof(a) << " " << sizeof(b) << " " << sizeof(c)<< " " << sizeof(d)<< " ";
+    // int a;
+    // float b;
+    // double c;
+    // unsigned char D = 'D';
+    // cout << sizeof("D") << " " << sizeof(D) << " ";
+    // cout << endl;
+    return 0;
+}
