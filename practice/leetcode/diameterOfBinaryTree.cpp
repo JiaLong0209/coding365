bfdbbbd5fe899eb98915518a5ba11bf297ef3400
Jialong0209 <smt02091395@gmail.com>
2022-11-12 22:02:05 +0800

    1112 leetcode

--- practice/leetcode/diameterOfBinaryTree.cpp
+++ practice/leetcode/diameterOfBinaryTree.cpp
@@ -3,17 +3,18 @@
 #include <cmath>
 using namespace std;
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+//   Definition for a binary tree node.
+
+class TreeNode {
+    public:
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr){};
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr){};
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right){};
+};
+
 class Solution {
 public:
     int maxDepth(TreeNode* root){
