9f5c1e0e5e9b1faba60d777f69708d353600b6ac
Jialong0209 <smt02091395@gmail.com>
2022-11-14 00:37:10 +0800

    1114 leetcode

---
+++ practice/leetcode/hard/binaryTreeMaximumPathSum.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <limits>
+using namespace std;
+
+
+class Solution {
+public:
+    int max3(int a, int b, int c){
+        return a>b ? a>c ? a : c : c > b ? c : b;  
+    }
+    int findMaxRoot(TreeNode* root){
+        if(root == NULL) return 0;
+        int left = findMaxRoot(root->left) + root->val;
+        int right = findMaxRoot(root->right) + root->val;
+        int result = 0;
+        return max3(left,right,0);
+    }
+    int maxPathSum(TreeNode* root) {
+        if(root == NULL) return -2e9;
+        int mid = findMaxRoot(root->left) + root->val + findMaxRoot(root->right);
+        int left = maxPathSum(root->left);
+        int right = maxPathSum(root->right);
+        
+        
+        return max3(mid,left,right);
+    }
+};
