7369daa1090bfa04579fdb4366f00696b4b0a82b
Jialong0209 <smt02091395@gmail.com>
2023-06-04 00:28:54 +0800

    C# test

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,16 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
+﻿using System;
 
-namespace ConsoleApp1{
-    class Program{
-        static void Main(string[] args){
-            string? firstName,lastName;   // Nullable type
-            Console.Write("Enter your first name: ");
-            firstName = Console.ReadLine();
-            Console.Write("Enter your last name: ");
-            lastName = Console.ReadLine();
-            Console.Write("Your full name is "+firstName+" "+lastName+"\n");
-            
-        }
+class Program{
+    static void Main (string[] args){
+        Console.WriteLine("hello world");
+        
     }
 }
