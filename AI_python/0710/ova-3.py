ae2f5a8ed5471012310c9146d011e99805cdd7cd
Jialong0209 <smt02091395@gmail.com>
2023-07-10 17:09:19 +0800

    230710 ova-3.py fix bug

--- AI_python/0710/ova-3.py
+++ AI_python/0710/ova-3.py
@@ -7,19 +7,23 @@ def getPage(url):
 
 url = "https://vipmbr.cpc.com.tw/mbwebs/ShowHistoryPrice_oil.aspx"
 data = {}
-columns = ['調價日期', '無鉛汽油92', '無鉛汽油95', '無鉛汽油98', '高級柴油', '二行程無鉛汽油', '煤油', '甲種漁船油(KL)', '乙種漁船油(KL)', '低硫燃料油(0.5%)(KL)', '甲種低硫燃料油/低硫鍋爐油(0.5%)(KL)', '特種低硫燃料油(KL)	', '低硫鍋爐油(1%)(KL)']
 page = getPage(url)
-for i,v in enumerate(page.select('span')[:-1]): 
-    index, text = i%13, v.text 
-    if(data.get(columns[index]) == None):
-        data[columns[index]]=[text]
-    else:
-        data[columns[index]].append(text)
+labels = [i.text for i in page.select('th')]
 
-# data[columns[0]] = data[columns[0]][:-1]
+for tr in page.select('#MyGridView tr')[1:]:
+    for i,v in enumerate(tr.select('span')):
+        data[labels[i]] = [v.text] if (data.get(labels[i]) == None) else [*data[labels[i]],v.text]
+
 df = pd.DataFrame.from_dict(data)
 print(df)
 df.to_csv('cpc.csv')
-# print(data)
 
 
+
+# labels = ['調價日期', '無鉛汽油92', '無鉛汽油95', '無鉛汽油98', '高級柴油', '二行程無鉛汽油', '煤油', '甲種漁船油(KL)', '乙種漁船油(KL)', '低硫燃料油(0.5%)(KL)', '甲種低硫燃料油/低硫鍋爐油(0.5%)(KL)', '特種低硫燃料油(KL)	', '低硫鍋爐油(1%)(KL)']
+# for i,v in enumerate(page.select('span')[:-1]): 
+#     index, text = i%13, v.text 
+#     if(data.get(labels[index]) == None):
+#         data[labels[index]]=[text]
+#     else:
+#         data[labels[index]].append(text)
