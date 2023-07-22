79fff2213b6c061b20274bfc223d1e2d486a0f49
Jialong0209 <smt02091395@gmail.com>
2023-07-22 21:22:33 +0800

    230722 dl02d_my.py Convolution 55%

---
+++ AI_python/AI/0721/dl02c_my.py
@@ -0,0 +1,39 @@
+from tensorflow import keras
+import tensorflow as tf
+import numpy as np
+import glob, os
+
+imgSize = (28,28)
+imgUtils = keras.preprocessing.image
+
+# 1. Loading model 
+model = keras.models.load_model('./AI/mnist_model')
+
+# 2. Checking the properties of model
+model.summary()
+
+def loadScaleImg(path):
+    return imgUtils.load_img(path, color_mode="grayscale", target_size=imgSize)
+
+def predictImage(path):
+    image = loadScaleImg(path)
+    image = imgUtils.img_to_array(image).reshape(imgSize)
+
+    x = image.reshape(1,28, 28, 1) / 255.0
+    answer = np.argmax(model.predict(x))
+    return answer
+
+base_path = './AI/src/mnist/'
+# predictImage('./AI/src/01.png')
+
+images = sorted([f for f in glob.glob(os.path.join(base_path, '*.png'))])
+# print(images)
+acc = np.array([])
+for i,image in enumerate(images):
+    predict = predictImage(image)
+    print(image)
+    print(f'predict: {predict} answer: {i%10}')
+    acc = np.append(acc, predict==i%10)
+
+print(acc)
+print(f'{round(len(acc[acc==True])/len(acc)*100,3)}%')
