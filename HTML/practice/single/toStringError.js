ab8f1ead735393abc3e1ef098f7837a4b808f511
Jialong0209 <smt02091395@gmail.com>
2023-08-01 19:13:35 +0800

    230801 toStringError.js

---
+++ HTML/practice/single/toStringError.js
@@ -0,0 +1,17 @@
+let a = 1;
+a = a.toString() // a = '1'
+console.log(typeof(a))
+c = '1'
+
+console.log(typeof(c))
+console.log(a, c)
+
+console.log(a==='1')
+console.log(c==='1')
+
+console.log(a==='1' && c==='1')
+
+let b = '34'
+b = toString()
+console.log(b)
+console.log(typeof (b))
