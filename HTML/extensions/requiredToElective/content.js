0e59320558fd029f2916c2b5cb1fd1a934d2d797
Jialong0209 <smt02091395@gmail.com>
2023-08-23 21:22:42 +0800

    230823 v1.0 requiredToElective extension

---
+++ HTML/extensions/requiredToElective/content.js
@@ -0,0 +1,13 @@
+table = document.querySelectorAll('div#main table')
+t = table[1]
+trs = t.children[0].children
+
+for(let i in trs){
+    if(i > 0 && i < trs.length){
+        for(let j in trs[i].children){
+            if(j == 9){
+                trs[i].children[j].innerHTML = `<font color="black">選修</font>`
+            }
+        } 
+    }
+}
