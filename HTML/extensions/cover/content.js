3c67ed94163f79ff0e4d77d0257c1b8b01bfaf63
Jialong0209 <smt02091395@gmail.com>
2023-08-25 23:55:37 +0800

    230825 create cover extension

---
+++ HTML/extensions/cover/content.js
@@ -0,0 +1,13 @@
+console.log('hello cover');
+
+var canvas = document.createElement("canvas");  
+var cover = document.querySelector('body');  
+canvas.id = "canvasLaLa2016";  
+cover.appendChild(canvas);  
+
+console.log(cover);
+var button = document.createElement("div");  
+button.id = "snow-button";  
+
+console.log(button)
+cover.appendChild(button);
