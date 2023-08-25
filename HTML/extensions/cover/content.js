2b72d065682e517503a0d53f03445d336c8490a5
Jialong0209 <smt02091395@gmail.com>
2023-08-26 00:08:56 +0800

    230826 v1.1.2 remove unnecessary parts in cover extension

--- HTML/extensions/cover/content.js
+++ HTML/extensions/cover/content.js
@@ -5,9 +5,7 @@ var cover = document.querySelector('body');
 canvas.id = "canvasLaLa2016";  
 cover.appendChild(canvas);  
 
-console.log(cover);
 var button = document.createElement("div");  
 button.id = "snow-button";  
 
-console.log(button)
 cover.appendChild(button);
