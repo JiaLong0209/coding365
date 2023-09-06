e579758da959a1e950ef9202658408c2e269f209
Jialong0209 <smt02091395@gmail.com>
2023-09-06 17:44:18 +0800

    230906 v0.2.0 add switchBoth function

--- HTML/extensions/googleTranslateVoice/content.js
+++ HTML/extensions/googleTranslateVoice/content.js
@@ -4,33 +4,42 @@ window.onload = () => {
     let voiceSelector = 'div.m0Qfkd span button';
     let textSelector = 'textarea.er8xn';
 
-    function focus(){
-        setTimeout(()=>{
+    function switchBoth() {
+        document.querySelector('.lRTpdf').click();
+    }
+
+    function focus() {
+        setTimeout(() => {
             document.querySelector(textSelector).focus()
         }, 1);
     }
 
-    function init(){
-        voiceButtons = [ document.querySelectorAll(voiceSelector)[0],  document.querySelectorAll(voiceSelector)[1]]
+    function init() {
+        voiceButtons = [document.querySelectorAll(voiceSelector)[0], document.querySelectorAll(voiceSelector)[1]]
     }
 
-    function keydownListener(e){
-        if(e.key == '`') {
+    function keydownListener(e) {
+        if (e.key == '`') {
             focus();
         }
-        if(!e.shiftKey && !e.altKey) return;
-        
-        switch(e.key.toLowerCase()){
+        if (!e.shiftKey && !e.altKey) return;
+
+        switch (e.key.toLowerCase()) {
+
             case 'j':
                 voiceButtons[0].click();
                 break;
-    
+
             case 'k':
                 voiceButtons[1].click();
                 break;
 
             case 'r':
                 init();
+                break;
+
+            case 's':
+                switchBoth();
                 break;
 
         }
