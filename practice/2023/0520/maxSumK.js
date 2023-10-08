d1c9e7fc7a23a42ffcad79648337fde522af6f8c
Jialong0209 <smt02091395@gmail.com>
2023-10-08 12:09:37 +0800

    231008 Remove unnecessary code in  maxSumK.js

--- practice/2023/0520/maxSumK.js
+++ practice/2023/0520/maxSumK.js
@@ -1,4 +1,4 @@
-let maxSum = (arr, k) => Math.max(...arr.map((n, index) => arr.slice(index, k + index)).map(n => n.reduce((cur, acc) => cur + acc)))
+let maxSum = (arr, k) => Math.max(...arr.map((_, index) => arr.slice(index, k + index)).map(n => n.reduce((cur, acc) => cur + acc)))
 
 let k = 3
 let arr = [3, 2, 4, 5, 1, 2, 50, 3, 6, 1, 100]
