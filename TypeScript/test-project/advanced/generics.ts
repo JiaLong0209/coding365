6e392464991bdf138a29d824c2aef2aaf303b495
JiaLong0209 <smt02091395@gmail.com>
2024-07-02 21:59:26 +0800

    240702 Add copyFileds function

--- TypeScript/test-project/advanced/generics.ts
+++ TypeScript/test-project/advanced/generics.ts
@@ -8,12 +8,17 @@ function array<T>(length: number, value: T): Array<T>{
 }
 
 
-console.log(array(10, 'x'));
-console.log(array(5, true));
-console.log(array(5, true));
+console.log(array<string>(10, 'x'));
+console.log(array<boolean>(5, true));
+
+function copyFields<T extends U, U>(a: T, b: U): T{
+    for(let key in b) a[key] = (<T>b)[key];
+    return a;
+}
 
 
+let a = {a: 1, b: 2, c: 3, d:4}
 
+console.log(copyFields(a, {a:10, b:100}))
 
-
-
+//console.log(copyFields(a, {a:10, b:100, f:1000})) //Error
