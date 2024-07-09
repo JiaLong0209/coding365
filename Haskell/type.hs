8f0e47c1cd7dbc4b4eab7c928c5751a8f10cf1a9
JiaLong0209 <smt02091395@gmail.com>
2024-07-09 21:30:28 +0800

    240709 Add show_read_ex function for practicing Haskell Show and Read types

--- Haskell/type.hs
+++ Haskell/type.hs
@@ -1,6 +1,5 @@
 #!/usr/bin/env runghc
 import Data.Typeable
-
 removeUppercase :: [Char] -> [Char]
 removeUppercase str = [x  | x <- str, x `elem` ['a'..'z']]
 
@@ -20,8 +19,28 @@ fac n = product [1..n]
 ex2 = do
     print(fac 19) 
     print(typeOf fac)
-    -- print(typeOf head [1,2,3,4])
+
+ex3 = do
+    print(5 == 5)
+    print(5 /= 5)
+    print(5 `compare` 3)
+    print(1 `compare` 3)
+    print(3 `compare` 3)
+
+show_read_ex = do
+    -- show: convert to string
+    print(show [1,2,3])
+    print(show "adfjoi")
+
+    -- read: string to other type
+    print(read "[1,2,3]" ++ [5,4])
+    print((read "4" :: Float) * 4)
+    print(read "(3, 'a')" :: (Double, Char))
 
 main = do 
     -- ex1
-    ex2
+    -- ex2
+    -- ex3
+    show_read_ex
+
+
