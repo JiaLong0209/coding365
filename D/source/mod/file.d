d2527bdbfde1ab8abbcce1b144a890cfcb5c9d9a
JiaLong0209 <smt02091395@gmail.com>
2024-08-23 23:10:28 +0800

    240823 Add .strip() to readText() output in file.d module
    - Remove leading and trailing whitespace from file contents
    - Improve robustness of file reading operations

--- D/source/mod/file.d
+++ D/source/mod/file.d
@@ -4,6 +4,7 @@ module mod.file;
 import std.conv;
 import std.file;
 import std.stdio;
+import std.string;
 
 void saveToFile(string filename, string data){
     File file = File(filename, "w");
@@ -11,15 +12,15 @@ void saveToFile(string filename, string data){
     file.writefln("%s", data);
     writefln("Write %s into file: %s", data, filename);
     file.close();
-    writefln("Close file: %s", filename);
+    writefln("Close file: %s\n", filename);
 }
 
 string loadFromFile(string filename){
-    return readText(filename);
+    return readText(filename).strip();
 }
 
 int loadIntFromFile(string filename){
-    return to!int(readText(filename));
+    return to!int(readText(filename).strip());
 }
 
 
