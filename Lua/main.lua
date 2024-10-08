24dc852f3045c9ded1b8603ae65716f7e8a59d7c
JiaLong0209 <smt02091395@gmail.com>
2024-10-08 23:31:13 +0800

    241008 Create Lua directory

---
+++ Lua/main.lua
@@ -0,0 +1,14 @@
+
+local math = require('math_utils')
+
+
+local sum = math.add(100, 50)
+local diff = math.substract(10, 50)
+
+print("Sum:", sum)
+print("Diff:", diff)
+
+
+
+
+
---
+++ Lua/math_utils.lua
@@ -0,0 +1,15 @@
+
+local math_utils = {}
+
+function math_utils.add(a, b)
+  return a + b
+end
+
+function math_utils.substract(a, b)
+  return a - b
+end
+
+return  math_utils
+
+
+
